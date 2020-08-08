a222cf6ab5e7d3f6614f3ca182b15b47616dcd42
Michael Olsen <molsen@Michaels-Air.lan>
2020-08-08 13:34:24 -0400

    sum done

--- src/recursion.js
+++ src/recursion.js
@@ -12,11 +12,11 @@ var factorial = function(n) {
   return n * factorial(n - 1)
 };
 
-console.log(factorial(5))
-
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (!array.length) return 0
+  return array[0] + sum(array.slice(1))
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
