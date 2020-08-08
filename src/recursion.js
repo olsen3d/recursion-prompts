f5de9fac699bb19921842ab24d519aa39d1da99d
Michael Olsen <molsen@Michaels-Air.lan>
2020-08-08 15:34:12 -0400

    exponent done

--- src/recursion.js
+++ src/recursion.js
@@ -22,21 +22,48 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) sum += arraySum(array[i])
+    else sum += array[i]
+  }
+  return sum
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  let result = true
+  if (n === 0) return result
+  !result
+  return isEven(n-1)
+
+  //come back to this
+
+
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n === 0) return 0
+  return (n < 0) ? (n + 1) + sumBelow(n + 1) : (n - 1) + sumBelow(n - 1)
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  // if (x > y) {
+  //   let temp = x
+  //   x = y
+  //   y = temp
+  // }
+  // const result = []
+  // result.push(x + 1)
+  // if (x + 1 === y - 1) return result
+  // return result.concat(range(x + 1, y))
+
+  //come back to this
 };
 
 // 7. Compute the exponent of a number.
@@ -45,6 +72,9 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) return 1
+  if (exp < 0) return exponent(base, exp + 1) / base
+  else return base * exponent(base, exp - 1)
 };
 
 // 8. Determine if a number is a power of two.
